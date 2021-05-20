74328bece6c776b1d81af7142373891fcfda084d
PriyanshuSingh-5 <singhprin05@gmail.com>
2021-05-20 07:17:44 +0530

    [Priyanshu] modified the connection to DB

---
+++ EmployeePayrollADO/EmployeeRepository.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeePayrollADO
+{
+    class EmployeeRepository
+    {
+        ///Specifying the connection string from the sql server connection.
+        public static string connectionString = @"Data Source=LAPTOP-NAVJ6800\SQLEXPRESS;Initial Catalog=Employee_payroll;Integrated Security=True";
+        /// Establishing the connection using the Sql
+        SqlConnection connection = new SqlConnection(connectionString);
+
+        /// <summary>
+        ///UC1 Creating a method for checking for the validity of the connection.
+        /// </summary>
+        public void EnsureDataBaseConnection()
+        {
+            this.connection.Open();
+            using (connection)
+            {
+                Console.WriteLine("The Connection is created");
+            }
+            this.connection.Close();
+        }
+    }
+}
+
+
--- EmployeePayrollADO/Program.cs
+++ EmployeePayrollADO/Program.cs
@@ -10,6 +10,13 @@ namespace EmployeePayrollADO
     {
         static void Main(string[] args)
         {
+           
+            Console.WriteLine("Welcome to Employee Payroll Services Using ADO.NET");
+            //Creating a instance object of EmployeeRepository class.
+            EmployeeRepository repository = new EmployeeRepository();
+            // UC1 Ensuring the database connection using the sql connection string
+            repository.EnsureDataBaseConnection();
+            Console.ReadLine();
         }
     }
 }
