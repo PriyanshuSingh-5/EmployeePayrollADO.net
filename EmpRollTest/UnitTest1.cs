f485630d31b9e9b1e6a47b7f33d34993f61525d7
PriyanshuSingh-5 <singhprin05@gmail.com>
2021-05-25 11:45:56 +0530

    [Priyanshu] used UnitTest for salary update

---
+++ EmpRollTest/UnitTest1.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using EmployeePayrollADO;
+using System;
+
+namespace EmpRollTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+       
+            /// UC 4 Given the update salary value check if the database got updated.
+            /// </summary>
+           
+            public void GivenUpdateSalaryValue_CheckIfTheDatabaseGotUpdated()
+            {
+                //Arrange
+                string empName = "Priyanshu";
+                double basicPay = 60000;
+                EmployeeRepository repository = new EmployeeRepository();
+                EmployeeModel empModel = new EmployeeModel();
+                //Act
+                repository.UpdateSalaryIntoDatabase(empName, basicPay);
+                double expectedPay = repository.UpdatedSalaryFromDatabase(empName);
+                //Assert
+                Assert.AreEqual(basicPay, expectedPay);
+            }
+     }
+}
+
